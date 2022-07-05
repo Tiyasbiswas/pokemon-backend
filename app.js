9d10c7d0d470b2c9e12cf5933acc05dde1916ec9
gigi <giovannaggs@gmail.com>
2022-07-05 16:27:35 +0200

    tuesday's update

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
  const jsonData= require("./data.json");
  const express = require("express");
 const cors=require("cors");
-const port=process.env.PORT || 8000;
+const port=process.env.PORT || 5000;
 const app= express();
 
 app.get("/", (req, res) =>
   res.send("Hello")
 );
 
+app.get("/about", (req, res) =>
+res.send(`<ul>
+  ${
+    jsonData.map(item => `
+      <li>
+        <p>${item.name.english}:${item.type}</p>
+      </li>`
+    )
+    .join("")
+  }
+  </ul>`
+));
+
+app.get("/pokemon/:id", (req, res) => {
+  res.send(jsonData.find(e => e.id == req.params.id)) 
+});
 
 
 app.listen(port, () => console.log(`started listening on port ${port}`));
