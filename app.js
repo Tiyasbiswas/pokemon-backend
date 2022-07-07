d2dc6b8a431452183b922dc5219ed366f97eb261
Tiyasbiswas <tiyasbiswas17@gmail.com>
2022-07-07 16:27:44 +0200

    typebased Query added

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
- const jsonData= require("./data.json");
- const express = require("express");
+const express = require("express");
 const cors=require("cors");
-const port=process.env.PORT || 8000;
+const jsonData= require("./data.json");
+
+const port=process.env.PORT || 8080;
 const app= express();
 
 app.get("/", (req, res) =>
   res.send("Hello")
 );
+app.get("/pokemon", (req, res) =>
+res.send(`<ul>
+  ${
+    jsonData.map(item => `
+      <li>
+        <p>${item.name.english}:${item.type}</p>
+      </li>`
+    )
+    .join("")
+  }
+  </ul>
+  `)
+);
+
+
+app.get("/pokemon/:id", (req, res) => {
+  res.send(jsonData.find(e => e.id == req.params.id)) 
+
+});
+
+ app.get("/pokemon/:pid/:p_type", function(req, res){
+  const pids=jsonData[req.params.pid-1];
+  console.log("-----------------",pids);
+  const pok_type=pids.type;
+  const names=pids.name.english;
+
+ // res.send(pok_type);
+// res.send({pok_type,names});
+res.send({names});
+
+
+ });
+
+
+
+
+
+
+
+
+
+
+
 
 
 
