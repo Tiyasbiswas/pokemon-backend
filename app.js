2a6616be84d93dfc59a7d8c093d00a0523dbe80c
Neha <nehashaikh470@gmail.com>
2022-07-11 17:17:12 +0200

    add cors

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
-const jsonData= require("./data.json");
 const express = require("express");
-const cors=require("cors");
-const port=process.env.PORT || 5000;
+const jsonData= require("./data.json");
+
+const port=process.env.PORT || 8080;
 const app= express();
 const cors = require('cors');
 app.use(cors())
 app.get("/", (req, res) =>
+  res.send("Hello")
+);
+app.get("/pokemon", (req, res) =>
 res.send(`<ul>
   ${
     jsonData.map(item => `
       <li>
-        <p>${item.name.english}</p>
-        <p>Type:${item.type}</p>
-        <p>Base:${item.base}</p>
+        <p>${item.name.english}:${item.type}</p>
       </li>`
     )
     .join("")
   }
-  </ul>`
-));
+  </ul>
+  `)
+);
+
 
 app.get("/pokemon/:id", (req, res) => {
   res.send(jsonData.find(e => e.id == req.params.id)) 
+
 });
+
+ app.get("/pokemon/:pid/:p_type", function(req, res){
+  const pids=jsonData[req.params.pid-1];
+  console.log("-----------------",pids);
+  const pok_type=pids.type;
+  const names=pids.name.english;
+
+ // res.send(pok_type);
+// res.send({pok_type,names});
+res.send({names});
+
+
+ });
+
+
+
+
+
+
+
+
+
+
+
 
 
 
