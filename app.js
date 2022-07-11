fba9becb68369b101a5e7732af0f8c00b571c8ae
Neha <nehashaikh470@gmail.com>
2022-07-11 17:11:17 +0200

    add cors

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
- const jsonData= require("./data.json");
- const express = require("express");
+const jsonData= require("./data.json");
+const express = require("express");
 const cors=require("cors");
-const port=process.env.PORT || 8000;
+const port=process.env.PORT || 5000;
 const app= express();
-
+const cors = require('cors');
+app.use(cors())
 app.get("/", (req, res) =>
-  res.send("Hello")
-);
+res.send(`<ul>
+  ${
+    jsonData.map(item => `
+      <li>
+        <p>${item.name.english}</p>
+        <p>Type:${item.type}</p>
+        <p>Base:${item.base}</p>
+      </li>`
+    )
+    .join("")
+  }
+  </ul>`
+));
+
+app.get("/pokemon/:id", (req, res) => {
+  res.send(jsonData.find(e => e.id == req.params.id)) 
+});
 
 
 
